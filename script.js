70fc57a2c6790e3d57b23089453fa75c992eec5f
Vitaliy Skoblin <Vitaliy_Skoblin@epam.com>
2020-03-15 16:58:46 +0400

    feat: add function to change slide into slider

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
-  function SelectMe(mainClass, container, target) {
-    const addedClass = mainClass + "-selected";
+
+  // Navigation bar
+
+  function selectMe(mainClass, container, target) {
+    const addedClass = mainClass.concat("-selected");
 
     container.forEach((obj) => {
       obj.classList.remove(addedClass);
@@ -16,6 +19,57 @@ window.onload = function() {
   const navigationLinks = navigation.querySelectorAll(".".concat(navigationLinkClass));
   
   navigation.addEventListener("click", (e) => {
-    SelectMe(navigationLinkClass, navigationLinks, e.target);
+    selectMe(navigationLinkClass, navigationLinks, e.target);
+  });
+
+  // Section-slider
+
+  const slider = document.querySelector(".section-slider");
+  const slides = slider.querySelectorAll(".slide");
+
+  function sliderReset() {
+    let index = 0;
+
+    slides.forEach((element, elementIndex) => {
+      if(element.classList.contains("slide-selected")) {
+        index = elementIndex;
+        element.classList.remove("slide-selected");
+      }
+    });
+
+    return index;
+  } 
+
+  function setSlideBackground(slideIndex) {
+    slider.classList.toggle("section-slider-red", slideIndex === 0);
+    slider.classList.toggle("section-slider-blue", slideIndex === 1);
+  }
+
+  function previousSlide() {
+    let index = sliderReset() - 1;
+
+    if(index < 0) {
+      index = slides.length - 1;
+    }
+
+    slides[index].classList.add("slide-selected");
+    setSlideBackground(index);
+  }
+
+  function nextSlide() {
+    let index = (sliderReset() + 1) % slides.length;
+    slides[index].classList.add("slide-selected");
+    setSlideBackground(index);
+  }
+
+  const sliderPreviousButton = document.querySelector(".previous-button");
+  const sliderNextButton = document.querySelector(".next-button");
+
+  sliderPreviousButton.addEventListener("click", () => {
+    previousSlide();
+  });
+
+  sliderNextButton.addEventListener("click", () => {
+    nextSlide();
   });
 }
